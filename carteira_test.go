622807630650f47ac733c76e642bf864ad9604ae
andamorim2206 <deds5879@gmail.com>
2024-12-23 21:22:11 -0300

    fix: ponteiros

---
+++ carteira_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCarteira(t *testing.T) {
+    t.Run("Depositar", func(t *testing.T) {
+        carteira := Carteira{}
+        carteira.Depositar(Bitcoin(10))
+
+        confirmaSaldo(t, carteira, Bitcoin(10))
+    })
+
+    t.Run("Retirar com saldo suficiente", func(t *testing.T) {
+        carteira := Carteira{Bitcoin(20)}
+        erro := carteira.Retirar(Bitcoin(10))
+
+        confirmaSaldo(t, carteira, Bitcoin(10))
+        confirmaErroInexistente(t, erro)
+    })
+}
+
+func confirmaSaldo(t *testing.T, carteira Carteira, esperado Bitcoin) {
+    t.Helper()
+    resultado := carteira.Saldo()
+
+    if resultado != esperado {
+        t.Errorf("resultado %s, esperado %s", resultado, esperado)
+    }
+}
+
+func confirmaErroInexistente(t *testing.T, resultado error) {
+    t.Helper()
+    if resultado != nil {
+        t.Fatal("erro inesperado recebido")
+    }
+}
+
+func confirmaErro(t *testing.T, resultado error, esperado error) {
+    t.Helper()
+    if resultado == nil {
+        t.Fatal("esperava um erro, mas nenhum ocorreu")
+    }
+
+    if resultado != esperado {
+        t.Errorf("erro resultado %s, erro esperado %s", resultado, esperado)
+    }
+}
